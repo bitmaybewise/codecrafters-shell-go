4ab4f016f0195ebc536b92d0a5fe2e2917d752cd
Hercules Merscher <hlmerscher@gmail.com>
2024-06-02 12:55:20 +0200

    Run a program

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -15,11 +17,11 @@ func main() {
 		value = strings.Trim(value, "\n")
 		cmd := strings.Split(value, " ")
 		shellFn, ok := shellBuiltins[cmd[0]]
-		if !ok {
-			fmt.Printf("%s: command not found\n", cmd[0])
+		if ok {
+			shellFn(cmd, shellBuiltins)
 			continue
 		}
-		shellFn(cmd, shellBuiltins)
+		shellExec(cmd)
 	}
 }
 
@@ -47,22 +49,46 @@ func shellType(cmd []string, builtins shellBuiltinsType) {
 		fmt.Printf("%s is a shell builtin\n", cmd[1])
 		return
 	}
-	osPath := os.Getenv("PATH")
-	paths := strings.Split(osPath, ":")
-	for _, dir := range paths {
-		files, err := os.ReadDir(dir)
-		if err != nil {
-			continue
-		}
-		for _, file := range files {
-			if file.Name() == cmd[1] {
-				fmt.Printf("%s is %s/%s\n", cmd[1], dir, cmd[1])
-				return
-			}
-		}
+
+	dir, err := findInPath(cmd[1])
+	if err != nil {
+		fmt.Printf("%s %s\n", cmd[1], err)
+		return
 	}
 
-	fmt.Printf("%s not found\n", cmd[1])
+	fmt.Printf("%s is %s/%s\n", cmd[1], dir, cmd[1])
+}
+
+func shellExec(cmd []string) {
+	_exec := func(executable string, args ...string) (string, error) {
+		_cmd := exec.Command(executable, args...)
+		output, err := _cmd.Output()
+		if err != nil {
+			return "", fmt.Errorf("%s: error=%q\n", cmd[0], err)
+		}
+		return string(output), nil
+	}
+
+	output, err := _exec(cmd[0], cmd[1:]...)
+	if err == nil {
+		fmt.Print(output)
+		return
+	} // ignore when exe not found
+
+	dir, err := findInPath(cmd[0])
+	if err != nil {
+		fmt.Printf("%s: command %s\n", cmd[0], err)
+		return
+	}
+	executable := fmt.Sprintf("%s/%s", dir, cmd[0])
+	args := cmd[1:]
+
+	output, err = _exec(executable, args...)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s", output)
 }
 
 func waitForUserInput() string {
@@ -78,4 +104,23 @@ func checkArgs(fn shellFunction, _n int) shellFunction {
 		// TO DO
 		fn(cmd, builtins)
 	}
+}
+
+func findInPath(cmd string) (string, error) {
+	osPath := os.Getenv("PATH")
+	paths := strings.Split(osPath, ":")
+
+	for _, dir := range paths {
+		files, err := os.ReadDir(dir)
+		if err != nil {
+			continue
+		}
+		for _, file := range files {
+			if file.Name() == cmd {
+				return dir, nil
+			}
+		}
+	}
+
+	return "", errors.New("not found")
 }
